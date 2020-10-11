ac1d3ca175e169a9d1d33b9e8a8afb7c80e41a5c
Juan <juanfernandezv6@gmail.com>
2020-10-11 06:21:25 -0300

    Add Manhattan Distance

---
+++ 6-kyu/manhattanDistance.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/52998bf8caa22d98b800003a
+
+function manhattanDistance(pointA, pointB){
+    return Math.abs(pointA[0] - pointB[0]) + Math.abs(pointA[1] - pointB[1]);
+}
