b39c0ae59a163444db84ca9630badc9866360a08
Juan <juanfernandezv6@gmail.com>
2020-10-24 15:27:13 -0300

    Add Stones on the Table

---
+++ 7-kyu/stonesOnTheTable.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5f70e4cce10f9e0001c8995a
+
+function solve(stones) {
+    let counter = 0;
+    
+    for(let i = 0; i < stones.length - 1; i++){
+      if(stones[i] === stones[i+1])
+        counter++;
+    }
+
+    return counter;
+}
